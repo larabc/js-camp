b57cde06bfa7590d8fa2492b7fd41b66cd8db8b4
Lara Bautista <47418073+larabc@users.noreply.github.com>
2025-12-02 10:49:25 +0100

    link & hooks useNavigate and useLocation of React Router

--- 03-router-and-zustand/src/components/Link.jsx
+++ 03-router-and-zustand/src/components/Link.jsx
@@ -1,30 +1,14 @@
-import { useRouter } from '../hooks/useRouter.jsx'
+import { Link as NavLink } from 'react-router'
 import styles from './Link.module.css'
 
 export function Link({ href, children, className = '', ...restOfProps }) {
-    const { navigateTo, currentPath } = useRouter()
-
-    const handleClick = (event) => {
-        event.preventDefault()
-        navigateTo(href)
-    }
-
-    const isActive = href === currentPath
-
-    const finalClassName = `
-        ${styles.link} 
-        ${isActive ? styles.active : ''} 
-        ${className}
-    `.trim()
-
     return (
-        <a
-            href={href}
-            onClick={handleClick}
-            className={finalClassName}
+        <NavLink
+            to={href}
+            className={`${styles.link} ${className}`}
             {...restOfProps}
         >
             {children}
-        </a>
+        </NavLink>
     )
 }
--- 03-router-and-zustand/src/hooks/useRouter.jsx
+++ 03-router-and-zustand/src/hooks/useRouter.jsx
@@ -1,26 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useNavigate, useLocation } from 'react-router'
 
 export function useRouter() {
-    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+    const navigate = useNavigate()
+    const location = useLocation()
 
-    useEffect(() => {
-        const handleLocationChange = () => {
-            setCurrentPath(window.location.pathname)
-        }
-        window.addEventListener('popstate', handleLocationChange)
-
-        return () => {
-            window.removeEventListener('popstate', handleLocationChange)
-        }
-    }, [])
 
     function navigateTo(path) {
-        window.history.pushState({}, '', path)
-        window.dispatchEvent(new PopStateEvent('popstate'))
+        navigate(path)
     }
 
     return {
-        currentPath,
+        currentPath: location.pathname,
         navigateTo
     }
 }
